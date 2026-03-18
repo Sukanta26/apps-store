cbbb810b70228b87b0a3ed258c9587910921d9cf
Sukanta Saha <sukantasaha262@email.com>
2026-03-19 05:47:15 +0600

    Added the navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Hello Ami Navbar</h1>
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
     </div>
   );
 };
