ef2df18b5d1dcc303c13e06b288a7d8392a67bb9
Sukanta Saha <sukantasaha262@email.com>
2026-03-19 23:08:13 +0600

    Added the Navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,28 +1,58 @@
 import React from "react";
+import navLogo from "../../assets/logo.png";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div className="bg-base-100 shadow-sm">
+      {/* Top Navbar */}
+      <div className="navbar max-w-7xl mx-auto px-4">
+        {/* Left - Logo */}
+        <div className="navbar-start">
+          <div className="flex items-center gap-2">
+            <img src={navLogo} alt="" className="w-10 h-10 object-contain" />
+            <a className="text-xl font-bold text-violet-700">HERO.IO</a>
+          </div>
+        </div>
+
+        {/* Center - Desktop Menu */}
+        <div className="navbar-center hidden md:flex">
+          <ul className="menu menu-horizontal px-1 font-medium">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">Apps</Link>
+            </li>
+            <li>
+              <Link to="/">Installation</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Right - Button */}
+        <div className="navbar-end">
+          <a className="btn text-white bg-gradient-to-br from-violet-700 to-violet-500 border-none flex items-center gap-2 hover:scale-105 hover:brightness-110 transition duration-300">
+            <FaGithub size={18} />
+            Contribute
+          </a>
         </div>
       </div>
-      <div className="navbar-center">
-        <ul className="menu menu-horizontal px-1">
+
+      {/* Mobile Menu (below navbar) */}
+      <div className="md:hidden px-4 pb-3">
+        <ul className="menu bg-base-200 rounded-box text-center font-medium">
           <li>
-            <a>Item 1</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a>Item 3</a>
+            <Link to="/">Apps</Link>
           </li>
           <li>
-            <a>Item 3</a>
+            <Link to="/">Installation</Link>
           </li>
         </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
       </div>
     </div>
   );
--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import Navbar from "../../components/Header/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../../components/Footer/Footer";
-import ErrorPage from "../ErrorPage/ErrorPage";
 
 const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Outlet>
-        <ErrorPage></ErrorPage>
-      </Outlet>
+      <Outlet></Outlet>
       <Footer></Footer>
     </div>
   );
